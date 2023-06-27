cd207291ad6b2a64dba215d90a3c7693f0a9131b
Madina Akhmedova <akhmedovamadina11@gmail.com>
2023-06-27 13:59:12 +0200

    hw-4 заготовка

---
+++ src/test/java/com/github/SelenideRepositorySearch.java
@@ -0,0 +1,16 @@
+package com.github;
+
+import org.junit.jupiter.api.Test;
+
+public class SelenideRepositorySearch {
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop() {
+
+        //открыть первую страницу
+        //ввести в поле поиска selenide и нажать enter
+        //кликнуть на первый репозиторий из списка найденных
+        //проверка: заголовка selenide/selenide
+
+    }
+}

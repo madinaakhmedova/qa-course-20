225e8d1e9e163fc84c75e6099bb1db77f81ab4b2
Madina Akhmedova <akhmedovamadina11@gmail.com>
2023-07-25 16:06:03 +0200

    hw_5

---
+++ src/test/java/com/github/hw5_1.java
@@ -0,0 +1,27 @@
+package com.github;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.channels.ConnectionPendingException;
+
+public class hw5_1 {
+
+
+    @Test
+    void clickHoverMenuSolution() {
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+    open("/text-box");
---
+++ src/test/java/com/github/hw5_2.java
@@ -0,0 +1,7 @@
+package com.github;
+
+import org.junit.jupiter.api.Test;
+
+public class hw5_2 {
+
+}

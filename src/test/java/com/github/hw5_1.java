401b50d127252ef44ed820f8735c15521e9d391b
Madina Akhmedova <akhmedovamadina11@gmail.com>
2023-07-27 13:02:00 +0200

    hw_5

--- src/test/java/com/github/hw5_1.java
+++ src/test/java/com/github/hw5_1.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 import java.nio.channels.ConnectionPendingException;
@@ -23,12 +25,20 @@ public class hw5_1 {
         // открыть страницу /
         open("/");
         //навести в меню на пункт Solutions
+        $(byText("Solutions")).hover();
 
         //в выпадающем меню Solutions найти элемент Enterprize и кликнуть на него
+        $(byText("Enterprise")).click();
+
+        //убедиться, что есть заголовок"Build like the best."
+        $(".enterprise-hero").shouldHave(text("Build like the best"));
+
+
+
+
         //убедиться, что есть заголовок"Build like the best."
 
-
-
+        sleep(2000  );
     }
 
 
